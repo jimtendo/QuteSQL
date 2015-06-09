b72f711dd6e6dec381bcd9c3ac4d16a30a1ecdf5
Jim Hamill <jim@optimising.com.au>
2015-06-09 23:23:31 +0200

    Various updates

---
+++ Extension/MySQLSchemaModel.cpp
@@ -0,0 +1,5 @@
+#include "MySQLSchemaModel.h"
+
+MySQLSchemaModel::MySQLSchemaModel()
+{
+}
---
+++ Extension/MySQLSchemaModel.h
@@ -0,0 +1,12 @@
+#ifndef MYSQLSCHEMAMODEL_H
+#define MYSQLSCHEMAMODEL_H
+
+#include <QSqlQueryModel>
+
+class MySQLSchemaModel : public QSqlQueryModel
+{
+public:
+    MySQLSchemaModel();
+};
+
+#endif // MYSQLSCHEMAMODEL_H
